1c594c7ca94b404f1be628f85323f0f9b38fc756
Pavol Mráz <pavol.mraz@fores.group>
2019-11-04 13:58:04 +0100

    EnhancedCasePolicyEditor - cascading parameters, nesting, event handling.

---
+++ RazorClassLibrary/Theme.cs
@@ -0,0 +1,20 @@
+using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RazorClassLibrary
+{
+  public class Theme
+  {
+    public string DefaultButtonCssClass { get; private set; } = "btn-success";
+
+
+    public async Task ChangeThemeAsync()
+    {
+      await Task.Yield();
+      this.DefaultButtonCssClass = this.DefaultButtonCssClass.Contains("success") ? "btn-primary" : "btn-success";
+    }
+  }
+}
